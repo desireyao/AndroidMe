f036b1ae59386153a2c06830fb572d49c141109f
desireyao <desireyao@163.com>
2016-11-08 18:08:54 +0800

    增加每日一题 TreeSet

---
+++ testjava/src/main/java/com/example/question/Ques1108.java
@@ -0,0 +1,54 @@
+package com.example.question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+/**
+ * Package com.example.question.
+ * Created by yaoh on 2016/11/08.
+ * Company Beacool IT Ltd.
+ * <p/>
+ * Description:
+ */
+public class Ques1108 {
+
+    public static void main(String[] args) {
+
+        Animal dog = new Animal("dog",1);
+        Animal cat = new Animal("cat",2);
+
+        TreeSet<Animal> animals = new TreeSet<Animal>();
+//        List<Animal> animals = new ArrayList<Animal>();
+        animals.add(dog);
+        animals.add(cat);
+        System.out.println(animals);  // 1
+    }
+}
+    // 动物类
+    class Animal implements Comparable<Animal>{
+        private String name;
+        private int order;
+        public int getOrder() {
+            return order;
+        }
+
+        public void setOrder(int order) {
+            this.order = order;
+        }
+
+        Animal(String name,int order){
+            this.name = name;
+            this.order = order;
+        }
+
+        @Override
+        public int compareTo(Animal animal){
+            return order;
+        }
+
+        @Override
+        public String toString(){
+            return name;
+        }
+}
