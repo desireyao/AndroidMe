ea83bd725c023b681de78e92735081e323ee3e04
desireyao <desireyao@163.com>
2016-11-08 18:07:51 +0800

    增加每日一题 构造函数顺序调用的测试

---
+++ testjava/src/main/java/com/example/question/Ques1014.java
@@ -0,0 +1,42 @@
+package com.example.question;
+
+import sun.applet.Main;
+
+/**
+ * Package com.example.question.
+ * Created by yaoh on 2016/11/08.
+ * Company Beacool IT Ltd.
+ * <p/>
+ * Description:
+ */
+public class Ques1014 {
+
+    public static void main(String[] args) {
+        new Obj();
+    }
+}
+
+class Obj {
+
+    Other other = new Other();
+
+    public Obj() {
+        this("name");
+        System.out.println("===obj===");
+    }
+
+    public Obj(String name) {
+       System.out.println("---obj---");
+    }
+}
+
+class Other{
+
+    public Other(){
+        System.out.println("---other---");
+    }
+}
+
+
+
+
