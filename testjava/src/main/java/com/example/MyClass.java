cc9df7bda78e9a5f7a6fe7b924783b61b429b665
desireyao <desireyao@163.com>
2016-11-08 13:50:54 +0800

    View Model 新增ButterKnife

--- testjava/src/main/java/com/example/MyClass.java
+++ testjava/src/main/java/com/example/MyClass.java
@@ -5,19 +5,25 @@ public class MyClass {
 //    private static Bank bank;
 
     public static void main(String[] args){
-        System.out.print("------------");
+        System.out.print("start: \n");
 //        bank = new Bank();
-        for (int i = 0; i < 10; i ++){
-            new Thread(new Task()).start();
-        }
+//        for (int i = 0; i < 10; i ++){
+//            new Thread(new Task()).start();
+//        }
+        testByte();
+
     }
 
     static class Task implements Runnable{
-
         @Override
         public void run() {
             Bank bank = new Bank();
             bank.getMoney();
         }
+    }
+
+    static void testByte(){
+        byte[] res = new byte[]{0x0,0x12,0x33,0x44};
+        System.out.print(res.length + "res[1]: " + (res[2] & 0xff));
     }
 }
--- view/src/main/java/com/yaoh/view/MainActivity.java
+++ view/src/main/java/com/yaoh/view/MainActivity.java
@@ -2,18 +2,37 @@ package com.yaoh.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout linearLayout;
-    private Button btn;
+
+//    @BindView(R.id.btn) Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ButterKnife.bind(this);
+
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.e("TAG","ONCLICK");
+//            }
+//        });
+    }
+
+    @OnClick({R.id.btn1,R.id.btn2})
+    public void OnClickItem(View view){
+        Log.e("TAG","OnClickItem/view:" + view);
     }
 }
