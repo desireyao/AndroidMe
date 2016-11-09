ba3a3a446beddf3b47c79b29ad3fc37c18d98ba4
desireyao <desireyao@163.com>
2016-11-09 16:43:24 +0800

    测试 SP

--- code/src/main/java/com/yaoh/code/MainActivity.java
+++ code/src/main/java/com/yaoh/code/MainActivity.java
@@ -3,11 +3,29 @@ package com.yaoh.code;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.blankj.utilcode.utils.LogUtils;
+import com.blankj.utilcode.utils.SPUtils;
+import com.blankj.utilcode.utils.StringUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test();
+    }
+
+    /**
+     * 测试
+     */
+    private void test(){
+
+        //测试中文转拼音
+        LogUtils.e(StringUtils.cn2PY("中国"));
+
+        // 测试 shareprefence
+        SPUtils spUtils = new SPUtils(this,"sp");
     }
 }
