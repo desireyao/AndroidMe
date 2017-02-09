26a59301bd1e5b3944994c168995fbf2b7285a77
desireyao <desireyao@163.com>
2017-02-09 10:52:28 +0800

    增加每日问题 0206

---
+++ testjava/src/main/java/com/example/question/Quest0206/Main.java
@@ -0,0 +1,11 @@
+package com.example.question.Quest0206;
+
+/**
+ * Package com.example.question.Quest0206.
+ * Created by yaoh on 2017/02/09.
+ * Company Beacool IT Ltd.
+ * <p/>
+ * Description:
+ */
+public class Main {
+}
---
+++ testjava/src/main/java/com/example/question/Quest0206/Person.java
@@ -0,0 +1,15 @@
+package com.example.question.Quest0206;
+
+/**
+ * Package com.example.question.Quest0206.
+ * Created by yaoh on 2017/02/09.
+ * Company Beacool IT Ltd.
+ * <p/>
+ * Description:
+ */
+public class Person {
+
+    private int b = 1;
+    private int a = b;
+
+}
